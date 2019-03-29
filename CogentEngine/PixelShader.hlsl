a199004327a5bd1a2fb517525442bf5df740e76d
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2019-03-28 20:41:46 -0400

    Cel Shading

--- CogentEngine/PixelShader.hlsl
+++ CogentEngine/PixelShader.hlsl
@@ -33,12 +33,25 @@ struct VertexToPixel
 
 float4 main(VertexToPixel input) : SV_TARGET
 {
+	float3 color = float3(1,1,1);
 	// Renormalize any interpolated normals
 	input.normal = normalize(input.normal);
 	float3 dirToLight = normalize(-dirLight.Direction);
 	float NdotL = dot(input.normal, dirToLight);
 	NdotL = saturate(NdotL);
+	float intensity = NdotL;
 	//float spec = calculateSpecular(input.normal, input.worldPos, dirToLight, cameraPosition) * roughness;
 	float3 totalLight = dirLight.DiffuseColor * NdotL + dirLight.AmbientColor;
-	return float4(totalLight,1);
+	color = color * totalLight;
+	// Discretize the intensity, based on a few cutoff points
+	if (intensity > 0.95)
+		color = 1 * color;
+	else if (intensity > 0.5)
+		color = 0.7 * color;
+	else if (intensity > 0.05)
+		color = 0.35 * color;
+	else
+		color = 0.1 * color;
+
+	return float4(color,1);
 }
