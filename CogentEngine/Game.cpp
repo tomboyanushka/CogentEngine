bc9f0ca9816826d0ea756cb2ce1039b1b572db19
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2019-09-09 00:08:44 -0400

    Updates & code clean up

--- CogentEngine/Game.cpp
+++ CogentEngine/Game.cpp
@@ -111,10 +111,10 @@ void Game::CreateMatrices()
 
 void Game::CreateBasicGeometry()
 {
-	sphere = new Mesh("../../Assets/Models/sphere.obj", device.Get(), commandList);
-	//sphere = mLoader.Load("../../Assets/Models/sphere.obj", device.Get(), commandList);
-	skyCube = new Mesh("../../Assets/Models/cube.obj", device.Get(), commandList);
-	//skyCube = mLoader.Load("../../Assets/Models/cube.obj", device.Get(), commandList);
+	//sphere = new Mesh("../../Assets/Models/sphere.obj", device.Get(), commandList);
+	sphere = mLoader.Load("../../Assets/Models/sphere.obj", device.Get(), commandList);
+	//skyCube = new Mesh("../../Assets/Models/cube.obj", device.Get(), commandList);
+	skyCube = mLoader.Load("../../Assets/Models/cube.obj", device.Get(), commandList);
 
 	for (int i = 0; i < numEntities; ++i)
 	{
@@ -479,7 +479,7 @@ void Game::Draw(float deltaTime, float totalTime)
 			DrawEntity(e);
 		}
 
-		//DrawSky();
+		DrawSky();
 
 	}
 
